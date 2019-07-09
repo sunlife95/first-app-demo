c136632f44cdf0180065ad598a8b8d47ca819cbb
sunlingfu <1298629573@qq.com>
2019-07-09 16:47:04 +0800

    二叉搜索树

---
+++ dataStructure/src/main/java/com/imooc/data/structure/tree/binarytree/BinarySearchTree.java
@@ -0,0 +1,11 @@
+package com.imooc.data.structure.tree.binarytree;
+
+/**
+ * @author sunlife
+ * @date 2019/7/9 16:45
+ */
+public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
+    private static class BinaryNode<AnyType>{
+
+    }
+}
