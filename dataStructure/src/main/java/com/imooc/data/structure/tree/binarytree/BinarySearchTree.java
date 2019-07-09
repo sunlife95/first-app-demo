ca040c92f12529ef301c6c15dad6375cb7866306
sunlingfu <1298629573@qq.com>
2019-07-09 16:50:19 +0800

    二叉搜索树 内部节点内部类

--- dataStructure/src/main/java/com/imooc/data/structure/tree/binarytree/BinarySearchTree.java
+++ dataStructure/src/main/java/com/imooc/data/structure/tree/binarytree/BinarySearchTree.java
@@ -6,6 +6,17 @@ package com.imooc.data.structure.tree.binarytree;
  */
 public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
     private static class BinaryNode<AnyType>{
+        BinaryNode(AnyType theElement) {
+            this(theElement, null, null);
+        }
+        BinaryNode(AnyType theElement, BinaryNode<AnyType> lt, BinaryNode<AnyType> rt) {
+            element = theElement;
+            left = lt;
+            right = rt;
+        }
 
+        AnyType element;
+        BinaryNode<AnyType> left;
+        BinaryNode<AnyType> right;
     }
 }
